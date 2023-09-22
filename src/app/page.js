c6bef7c3a69e4c73fbd45c27abd2a9d44611ff8c
Ricky004 <tanaydam000@gmail.com>
2023-09-22 18:04:03 +0530

    expandable sidebar [step-1]

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 "use client"
 
+import {  LayoutDashboard, Package, UserCircle } from "lucide-react"
+import { Sidebar, SidebarItem } from "./Sidebar"
 import Todo from "./Todo"
 
 export default function Home() {
 
   return (
     <>
-      <div className="flex justify-center h-screen w-full">
+      <div className="flex h-screen w-full">
+        <Sidebar>
+          <SidebarItem icon={<LayoutDashboard size={20}/>} text="Dashboard" active />
+          <SidebarItem icon={<UserCircle size={20}/>} text="Profile" />
+          <SidebarItem icon={<Package size={20}/>} text="Boxes" />
+          <SidebarItem icon={<Package size={20}/>} text="Boxes" />
+        </Sidebar>
         <Todo></Todo>
       </div>
     </>
